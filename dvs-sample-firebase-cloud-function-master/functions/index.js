bd7b0f6f02cbef80d934822855d464884a40945f
unknown <cmgrant@uno.edu>
2021-07-27 13:32:55 -0500

    First Commit

---
+++ dvs-sample-firebase-cloud-function-master/functions/index.js
@@ -0,0 +1,50 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const https = require('https');
+
+// Server configuration
+const token = "sk_3d1946f8-5989-446d-b357-d0f1df2f4dde";
+ 
+
+const app = express();
+
+app.use(express.json());
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+
+app.post('/ValidationRequests/complete', function (req, res) {
+    let data = JSON.stringify({})
+    
+    const options = {
+      host: "dvs2.idware.net",
+      port: 443,
+      path: "/api/v3/Verify/" + req.body.requestId,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json-patch+json",
+        "Content-Length": data.length,
+        Authorization: "Bearer " + token,
+      },
+    };
+
+    let chunks = '';
+    const httpreq = https.request(options, function (response) {
+        
+        response.setEncoding('utf8');
+        
+        response.on('data', function (chunk) {
+            chunks = chunks + chunk
+        });
+
+        response.on('end', function() {
+            let data = JSON.parse(chunks)
+            res.json(data)
+        })
+    });
+    httpreq.write(data);
+    httpreq.end();
+});
+
+exports.api = functions.https.onRequest(app);
